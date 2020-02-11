077605eab22f27c3587d1e105d583a2e3b7138c0
Mac-User <mac-user@makku-yuzanoMacBook-puro.local>
2020-02-11 22:50:10 +0900

    タスク9_動画教材ページ_ページネーション完了
    movies/index.html.erb修正、moviesモデル追記、db/csv_data/csv_data.csv追加
    
    movies/index.html.erb修正、moviesモデル追記、db/csv_data/csv_data.csv追加
    
    tasks/import_csv.rake追加
    
    Revert "tasks/import_csv.rake追加"
    
    This reverts commit 15ed095273e68919858531ca73c546720cdb4d87.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,20 @@
 class Movie < ApplicationRecord
+
+  def self.import(path)
+    list = []
+
+    CSV.foreach(path, headers: true) do |row|
+     list << {
+      name: row["name"],
+      url: row["url"]
+      }
+    end
+  
+    puts "インポート開始"
+    Movie.create!(list)
+    puts "インポートに成功しました"
+    rescue ActiveModel::UnknownAttributeError => invalid
+    puts "インポートに失敗しました:#{invaild}"
+  end
+
 end
