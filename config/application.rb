57796df2b96aa1338642ac410dc5a4b7d56ac3d4
Mac-User <mac-user@makku-yuzanoMacBook-puro.local>
2020-02-28 02:08:19 +0900

    タスク10_質問集ページの追加

--- config/application.rb
+++ config/application.rb
@@ -20,14 +20,12 @@ Bundler.require(*Rails.groups)
 module GyakutenCloneGroup8
   class Application < Rails::Application
     config.load_defaults 5.2
-    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     config.generators.system_tests = nil
     # 多言語対応
